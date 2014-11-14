a7da5c62db9fa28fb0276a74767748406a3a4c41
Magneticdud <Magneticdud@users.noreply.github.com>
2014-11-14 11:33:52 +0100

    Indentazione + end

--- enjoy.pas
+++ enjoy.pas
@@ -1,15 +1,17 @@
 program enjoy;
 uses crt;
 var cod:real;
+
 begin
-clrscr;
-sound (4000);
-repeat
-textcolor (red+blink);
-writeln ('Suonino sega per spegnerlo inserisci il codice');
-readln (cod);
-until cod=42369;
-textcolor (green + blink);
-writeln ('Ciao');
-nosound;
-readln
+  clrscr;
+  sound (4000);
+  repeat
+    textcolor (red+blink);
+    writeln ('Suonino sega per spegnerlo inserisci il codice');
+    readln (cod);
+  until cod=42369;
+  textcolor (green + blink);
+  writeln ('Ciao');
+  nosound;
+  readln;
+end.
